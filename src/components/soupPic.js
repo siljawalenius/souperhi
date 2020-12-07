dce8ecbf032e6dd3fa021a6bca0e6a4d7e73317c
Silja Walenius <siljawalenius@gmail.com>
2020-12-06 19:28:17 -0500

    intial classes and planning

---
+++ src/components/soupPic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SoupPic extends React.Component{
+    render(){
+        return(
+            <div className = "pic">
+                Hello!
+            </div>
+        )
+    }
+}
+
+export default SoupPic 
