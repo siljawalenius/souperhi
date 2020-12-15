2de734d8bfc8d5d28110b281817cfdea78f2126f
Silja Walenius <siljawalenius@gmail.com>
2020-12-14 23:37:16 -0500

    next steps: add in  react router

---
+++ src/components/allSoups.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./allsoups.css";
+
+class AllSoups extends React.Component {
+  render() {
+    return (
+      <div className="all">
+        display all soups here
+      </div>
+    );
+  }
+}
+
+export default AllSoups;
---
+++ src/getSoups.js
@@ -0,0 +1,8 @@
+//should I use an API or just have the values
+// tbh, probably an API
+//look into options where people can submit their own maybe?
+//or to start, just like the 3 I wrote I guess
+
+//pull data from API
+//save as objects
+//pass into appropriate parts of recipe page
